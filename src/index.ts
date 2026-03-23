6cbf0ad5a2e2b0af0cee1cc15884d3a924ab058f
tprei <thiagopreischadt@gmail.com>
2026-03-23 15:51:58 +0000

    chore: add project files and comprehensive gitignore
    - Add src/, package files, Dockerfile, fly.toml
    - Add entrypoint.sh and .env.example
    - Add git credential helper scripts
    - Ignore .npmrc and WSL mount artifacts

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import { createMinion, configFromEnv } from "@tprei/telegram-minions"
+
+const minion = createMinion({
+  ...configFromEnv(),
+  repos: {
+    "minions": "https://github.com/tprei/telegram-minions",
+  },
+})
+
+process.on("SIGTERM", () => { minion.stop(); process.exit(0) })
+process.on("SIGINT", () => { minion.stop(); process.exit(0) })
+
+process.on("uncaughtException", (err) => {
+  process.stderr.write(`uncaught exception: ${err}\n`)
+  process.exit(1)
+})
+
+process.on("unhandledRejection", (reason) => {
+  process.stderr.write(`unhandled rejection: ${reason}\n`)
+  process.exit(1)
+})
+
+await minion.start()
