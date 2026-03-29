7e858957e64d5a9992543f354ba732fde9edf020
tprei <thiagopreischadt@gmail.com>
2026-03-29 15:14:27 +0100

    feat: initialize Sentry error reporting at startup
    Adds @sentry/node dependency and integrates Sentry error capture into the
    application lifecycle. Initializes Sentry using config.sentry.dsn, wires
    captureException into uncaught exception and unhandled rejection handlers,
    and ensures error data is flushed before process exit.

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,27 @@
 import { createMinion, configFromEnv } from "@tprei/telegram-minions"
+import { initSentry, captureException, flush as flushSentry } from "@tprei/telegram-minions/dist/sentry.js"
+
+const config = configFromEnv()
+await initSentry(config.sentry?.dsn)
 
 const minion = createMinion({
-  ...configFromEnv(),
+  ...config,
   repos: {
     "minions": "https://github.com/tprei/telegram-minions",
   },
 })
 
-process.on("SIGTERM", () => { minion.stop(); process.exit(0) })
-process.on("SIGINT", () => { minion.stop(); process.exit(0) })
+process.on("SIGTERM", () => { minion.stop(); flushSentry().finally(() => process.exit(0)) })
+process.on("SIGINT", () => { minion.stop(); flushSentry().finally(() => process.exit(0)) })
 
 process.on("uncaughtException", (err) => {
-  process.stderr.write(`uncaught exception: ${err}\n`)
-  process.exit(1)
+  captureException(err, { handler: "uncaughtException" })
+  flushSentry().finally(() => process.exit(1))
 })
 
 process.on("unhandledRejection", (reason) => {
-  process.stderr.write(`unhandled rejection: ${reason}\n`)
-  process.exit(1)
+  captureException(reason, { handler: "unhandledRejection" })
+  flushSentry().finally(() => process.exit(1))
 })
 
 await minion.start()
